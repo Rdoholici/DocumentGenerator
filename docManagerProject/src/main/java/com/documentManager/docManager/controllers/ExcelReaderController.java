c720f39290fb7054ea8b5fb307332f26fe81db80
Doho <rdoholici@gmail.com>
2020-12-11 16:59:09 +0200

    excel reader

--- docManagerProject/src/main/java/com/documentManager/docManager/controllers/ExcelReaderController.java
+++ docManagerProject/src/main/java/com/documentManager/docManager/controllers/ExcelReaderController.java
@@ -1,5 +1,6 @@
 package com.documentManager.docManager.controllers;
 
+import com.documentManager.docManager.models.JiraTicket;
 import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
 import org.apache.poi.ss.usermodel.*;
 
@@ -10,25 +11,32 @@ import java.util.List;
 
 public class ExcelReaderController {
 
-    public static void main(String[] args) throws IOException, InvalidFormatException {
-        Workbook workbook = WorkbookFactory.create(new File("jira_excel_example.xlsx"));
+    /**
+     * Method to get jira tickets from a provided excel file.
+     * @param filePath the location of the excel file.
+     * @return a list of jira tickets serialized from the excel file.
+     * @throws IOException
+     * @throws InvalidFormatException
+     */
+    public static List<JiraTicket> extractJiraTicketsFromExcelFile(String filePath) throws IOException, InvalidFormatException {
+
+        List<JiraTicket> toReturn = new ArrayList<>();
+        Workbook workbook = WorkbookFactory.create(new File(filePath));
         Sheet sheet = workbook.getSheetAt(0);
         DataFormatter dataFormatter = new DataFormatter();
-//        https://www.callicoder.com/java-read-excel-file-apache-poi/
 
-        List<String> tickets = new ArrayList<>();
-        for (Row row : sheet) {
-//    System.out.println(row.getCell(5));
-//    System.out.println(row.getCell(5).equals("In testing"));
-            if (dataFormatter.formatCellValue(row.getCell(5)).equals("In testing") ) {
-                tickets.add(dataFormatter.formatCellValue(row.getCell(1)));
-            }
+        //go through each row in the sheet
+        for (int i = 1; i < sheet.getPhysicalNumberOfRows(); i++) {
+            toReturn.add(
+                    new JiraTicket(
+                            dataFormatter.formatCellValue(sheet.getRow(i).getCell(0)),
+                            dataFormatter.formatCellValue(sheet.getRow(i).getCell(1)),
+                            dataFormatter.formatCellValue(sheet.getRow(i).getCell(2)),
+                            dataFormatter.formatCellValue(sheet.getRow(i).getCell(4)),
+                            dataFormatter.formatCellValue(sheet.getRow(i).getCell(3)),
+                            dataFormatter.formatCellValue(sheet.getRow(i).getCell(5))
+                    ));
         }
-
-        tickets.forEach(
-                ticket -> System.out.println(ticket)
-        );
-
-
+        return toReturn;
     }
 }
