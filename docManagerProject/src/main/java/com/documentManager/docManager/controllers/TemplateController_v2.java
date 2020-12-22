885aa014eb556d753bc86a071c109fb0884d9385
Marius Marinescu <marinescumarius00@gmail.com>
2020-12-22 13:19:04 +0200

    replaceTextInParagraph

---
+++ docManagerProject/src/main/java/com/documentManager/docManager/controllers/TemplateController_v2.java
@@ -0,0 +1,82 @@
+package com.documentManager.docManager.controllers;
+
+import com.documentManager.docManager.models.JiraTicket;
+import com.documentManager.docManager.services.JiraService;
+import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
+import org.apache.poi.xwpf.usermodel.XWPFDocument;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.print.Doc;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.*;
+
+@RestController
+public class TemplateController_v2 {
+
+
+    public static void main(String[] args) throws IOException, InvalidFormatException {
+        DocumentGeneratorController.setDocumentTemplate("ter_template.docx");
+        HashMap<String, String> newValue = new HashMap<>();
+        newValue.put("RELEASE_NO", "N1234");
+        newValue.put("NAR_ID", "mockNar");
+        replaceKeywordsInTemplate(newValue);
+        cleanUpTag();
+        DocumentGeneratorController.saveDocument("modified2.docx");
+    }
+    @Autowired
+    JiraService jiraService;
+
+
+
+    public TemplateController_v2() throws IOException {
+    }
+
+    public static void replaceKeywordsInTemplate(HashMap<String,String> newValue) throws IOException, InvalidFormatException { //newValue ar fi valoarea introdusa de la tastatura
+
+        for ( String keyword : newValue.keySet()) {
+            DocumentGeneratorController.replaceTextInParagraph(keyword, newValue.get(keyword));
+        }
+
+    }
+    public static void cleanUpTag() {
+        DocumentGeneratorController.replaceTextInParagraph("<change>", "");
+
+    }
+
+    public void updateTableFromExcelOrApi (String filePath,String id, String tableHeader) throws IOException, InvalidFormatException {
+
+
+        List<JiraTicket> jiraTickets=new ArrayList<>();
+        //filePath ia valoarea documentului din UI
+        if(filePath != null){
+        jiraTickets= ExcelReaderController.extractJiraTicketsFromExcelFile(filePath);
+        } else {
+            jiraTickets= Arrays.asList(jiraService.getJiraTicketsById(id));
+        }
+        addRowOfJiraTicketsForId(jiraTickets,tableHeader);
+
+    }
+
+    private void addRowOfJiraTicketsForId(List<JiraTicket> jiraTickets, String tableHeader) { //tableHeader vine din UI
+        for (JiraTicket jiraTicket : jiraTickets) {
+            List<String> rowCells = new ArrayList<>();
+            rowCells.add(jiraTicket.getIssueType());
+            rowCells.add(jiraTicket.getIssue_key());
+            rowCells.add(jiraTicket.getDescription());
+            rowCells.add(jiraTicket.getPriority());
+            rowCells.add(jiraTicket.getSeverity());
+            rowCells.add(jiraTicket.getStatus());
+            DocumentGeneratorController.addRowToTable(tableHeader, rowCells);
+        }
+    }
+
+//    public void generateTer(String newValue, String filePath, String id, String tableHeader) throws IOException, InvalidFormatException {
+//        replaceKeywordsInTemplate(newValue);
+//        updateTableFromExcelOrApi(filePath, id, tableHeader);
+//        DocumentGeneratorController.saveDocument("modified.docx");
+//    }
+
+}
--- docManagerProject/src/main/java/com/documentManager/docManager/controllers/WebController.java
+++ docManagerProject/src/main/java/com/documentManager/docManager/controllers/WebController.java
@@ -18,11 +18,10 @@ public class WebController {
     JiraService jiraService;
 
     @RequestMapping("/exampleUI")
-    private String getConverterLength(Model model) {
+    private String getUiInfo(Model model) {
         UIModelInfo modelInfo = new UIModelInfo();
         model.addAttribute("modelInfo", modelInfo);
-//        Date dateAcum = new Date();
-//        model.addAttribute("datadeacum", dateAcum.toString());
+
         return "indexuri";
 
 
@@ -30,7 +29,7 @@ public class WebController {
     @RequestMapping(value = "/exampleUI", method = RequestMethod.POST)
 //    @ResponseBody
 
-    private String returnNarID(@RequestParam(value = "narID") String narID, Model model) {
+    private String returnUpdatedTER(@RequestParam(value = "narID") String narID, Model model) {
         System.out.println("111111 " + narID);
         List<JiraTicket> listOfJiraTicket= Arrays.asList(jiraService.getJiraTicketsById(narID));
         model.addAttribute("JiraTickets", listOfJiraTicket);
