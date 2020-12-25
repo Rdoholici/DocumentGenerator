cb4655188ced799a41ef7cc3632b04d5b9cf1cc1
florin <kirk_metalhead@yahoo.com>
2020-12-25 22:03:26 +0200

    added the predefined template logic captured keyword inputs into a third page - without excel headers small arrangements on some pages

--- docManagerProject/src/main/java/com/documentManager/docManager/controllers/MainController.java
+++ docManagerProject/src/main/java/com/documentManager/docManager/controllers/MainController.java
@@ -12,12 +12,13 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Objects;
 import java.util.UUID;
 
@@ -27,7 +28,10 @@ public class MainController {
 
     //file location
     private final String UPLOAD_DIR = "./uploads/";
+    private String TEMPL_UPLOAD_DIR = "./uploads/templates";
+
     private Path path;
+    private Path templPath;
     //variable for file name manipulation
 
     UUID randId = UUID.randomUUID();
@@ -58,11 +62,18 @@ public class MainController {
     }
 
     @GetMapping("/")
-    public String homepage() {
+    public String homepage(Model model) {
+
+        DocumentType documentType =new DocumentType();
+        model.addAttribute("documentType",documentType);
+
+        List<String> listType = Arrays.asList("TER - release","TER - Hot Fix");
+        model.addAttribute("listType", listType);
         return "index";
     }
 
-    @PostMapping("/page1")
+
+    @PostMapping("/page1") //for new ter from user
     public String uploadFile(@RequestParam("file") MultipartFile file, RedirectAttributes attributes, Model model) throws IOException {
 
         // check if file is empty
@@ -95,6 +106,9 @@ public class MainController {
         FileInputStream fis = new FileInputStream(docxDoc.getAbsolutePath());
         XWPFDocument document = new XWPFDocument(fis);
 
+        UserKeyword userKeyword = new UserKeyword();
+        model.addAttribute("userKeyword",userKeyword);
+
         // set the progressbar and errorflag and return success message
         model.addAttribute("progressBar",progressBar);
         model.addAttribute("errorFlag",errorFlag);
@@ -102,6 +116,66 @@ public class MainController {
         model.addAttribute("keywords", DocumentParser.getKeyWords(document, "<change>"));
         model.addAttribute("tableHeaders", DocumentParser.getTableHeaders(document));
         return "page1";
+    }
+
+    @GetMapping("/result")
+    public String showResult(Model model) {
+        UserKeyword userKeyword = new UserKeyword();
+        model.addAttribute("userKeyword",userKeyword);
+        return "result";
+}
+
+
+    @PostMapping("/result")
+    public String submitForm(@ModelAttribute("userKeyword") UserKeyword userKeyword, Model model) {
+        progressBar = 100;
+        model.addAttribute("progressBar",progressBar);
+        System.out.println(userKeyword);
+        return "result";
+    }
+
+    @PostMapping("/existingTemplate")
+    public String submitForm(@ModelAttribute("documentType") DocumentType documentType,Model model) throws IOException {
+
+
+
+        progressBar = 50;
+        model.addAttribute("progressBar",progressBar);
+        if(documentType.getName().equals("TER - release")) {
+            templPath = Paths.get(TEMPL_UPLOAD_DIR + "\\ter_release.docx");
+            System.out.println(templPath);
+            System.out.println("OK");
+        }
+        else if(documentType.getName().equals("TER - Hot Fix")) {
+            templPath = Paths.get(TEMPL_UPLOAD_DIR + "\\ter_hotfix.docx");
+            System.out.println(templPath);
+            System.out.println("OK");
+        }
+
+        File docxDoc = new File(templPath.toString());
+        FileInputStream fis = new FileInputStream(docxDoc.getAbsolutePath());
+        XWPFDocument document = new XWPFDocument(fis);
+
+        String fileName = StringUtils.cleanPath(Objects.requireNonNull(docxDoc.getName()));
+        errorFlag = true;
+
+        UserKeyword userKeyword = new UserKeyword();
+
+        model.addAttribute("userKeyword",userKeyword);
+        model.addAttribute("errorFlag",errorFlag);
+        model.addAttribute("keywords", DocumentParser.getKeyWords(document, "<change>"));
+        model.addAttribute("tableHeaders", DocumentParser.getTableHeaders(document));
+        model.addAttribute("message","You successfully selected " + fileName + " template!");
+
+// testare ca documentul este bine selectat
+//        List<XWPFParagraph> data = document.getParagraphs();
+//
+//        for(XWPFParagraph p : data) {
+//            System.out.print(p.getText());
+//        }
+//
+//        System.out.println(docxDoc.length());
+        return "extempresult";
     }
 }
 
